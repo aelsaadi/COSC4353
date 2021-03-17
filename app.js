e0931869241ef95d474a3f6347dc293f87fae24b
Ahmad Elsaadi <43180800+aelsaadi@users.noreply.github.com>
2021-03-17 14:04:31 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//Imports
+const express  = require('express')
+const app = express()
+const port = 3000
+
+
+// Static Files 
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+//Renders HTML file
+app.get('', (req,res) =>{
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+
+
+//Listen on port 3000
+app.listen(port,() => console.info('Listening on port ' + port))
