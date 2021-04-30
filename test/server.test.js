9f7ade12d6c29854a107050ee76f1fabe60fbadc
Ahmad Elsaadi <43180800+aelsaadi@users.noreply.github.com>
2021-04-30 15:11:32 -0500

    Add files via upload

---
+++ test/server.test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const user = require('../server');
+
+describe('User Test cases', ()=>{
+    it('should add new user',()=>{
+
+        console.log('added a new user');
+    })
+})
+describe('password Test cases', ()=>{
+    it('should add new password',()=>{
+
+        console.log('added a new password');
+    })
+})
+describe('ecryped password Test cases', ()=>{
+    it('should encrypt new password',()=>{
+
+        console.log('added a new encrypted password');
+    })
+})
+describe('ecryped password Test cases', ()=>{
+    it('should encrypt new password',()=>{
+
+        console.log('added a new encrypted password');
+    })
+})
